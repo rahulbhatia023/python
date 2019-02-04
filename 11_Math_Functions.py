4c39668873c02795763c5fabd3f049b258ce4361
rahulbhatia023 <mary291case23>
2019-02-04 13:44:36 +0530

    ..

---
+++ 11_Math_Functions.py
@@ -0,0 +1,16 @@
+import math  # It provides access to the mathematical functions defined by the C standard.
+
+print(math.sqrt(25))
+# 5.0
+
+print(math.floor(2.9))
+# 2
+
+print(math.ceil(2.2))
+# 3
+
+print(math.pow(3, 2))
+# 3.0
+
+print(math.pi)
+# 3.141592653589793
---
+++ 12_Import_Modules.py
@@ -0,0 +1,13 @@
+import math as m
+
+print(m.sqrt(25))
+# 5
+
+# from math import ceil
+
+# from math import ceil, floor
+
+# from math import *
+
+# from math import ceil as c, floor as f
+
